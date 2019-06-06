93440c110d7ce0873239a50efa7b39142a6c68f4
subayadhav <suba10499@gmail.com>
2019-06-06 09:37:42 +0530

    Update set210.py

--- set210.py
+++ set210.py
@@ -1,7 +1,7 @@
 a1,b=map(str,input().split())
 count=0
-for i in range(0,len(a)):
-  if(a[i]!=b[i]):
+for i in range(0,len(a1)):
+  if(a1[i]!=b[i]):
     count=count+1
 if(count==1):
   print("yes")
