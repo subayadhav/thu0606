90edfd4a83a335ced53f4d99e547826238e50bd2
subayadhav <suba10499@gmail.com>
2019-06-06 10:02:06 +0530

    Create 19.py

---
+++ 19.py
@@ -0,0 +1,18 @@
+d=int(input())
+e1=2
+l=[]
+v=[]
+for e1 in range(e,d+1):
+  i=2
+  while i<e1:
+    if e%i==0:
+      break
+    i=i+1
+  if e1==i:
+    l.append(e1)
+for i in l:
+  if d%i==0:
+    v.append(i)
+for i in range(0,len(v)-1):
+  print(v[i],end=" ")
+print(v[len(v)-1])
